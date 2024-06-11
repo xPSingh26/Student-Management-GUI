d5a68690dd72569b1d907dba72089389e3215e2a
Praval <p.singhx26@gmail.com>
2024-06-11 20:46:11 +0530

    Add menu file and help bar

---
+++ main.py
@@ -0,0 +1,22 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
+from  PyQt6.QtGui import QAction
+import sys
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Student Management System")
+
+        fileMenu = self.menuBar().addMenu("&File")
+        helpMenu = self.menuBar().addMenu("&Help")
+        fileMenuAction = QAction("Add Student", self)
+        fileMenu.addAction(fileMenuAction)
+        helpMenuAction = QAction("About", self)
+        helpMenu.addAction(helpMenuAction)
+
+
+app = QApplication(sys.argv)
+appWindow = MainWindow()
+appWindow.show()
+sys.exit(app.exec())
